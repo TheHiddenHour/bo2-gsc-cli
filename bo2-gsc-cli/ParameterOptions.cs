baa57fe6341b7d08b5539349576090bc04a05e60
TheHiddenHour <josheze.duque@gmail.com>
2020-04-22 18:45:33 -0700

    Updated README.md and cleaned parameter descriptions

--- bo2-gsc-cli/ParameterOptions.cs
+++ bo2-gsc-cli/ParameterOptions.cs
@@ -2,7 +2,7 @@
 
 namespace bo2_gsc_cli {
     class ParameterOptions {
-        [Option('a', "api", HelpText = "PS3 API to use for reseting the ScriptParseTree and injection")]
+        [Option('a', "api", HelpText = "PS3 API to use for resetting the ScriptParseTree or injection")]
         public string SelectedPS3API { get; set; }
 
         [Option('g', "gametype", HelpText = "Gametype to reset the ScriptParseTree or inject for")]
@@ -11,13 +11,13 @@ namespace bo2_gsc_cli {
         [Option('r', "reset", SetName = "reset", HelpText = "Reset the ScriptParseTree")]
         public bool ResetScriptParseTree { get; set; }
 
-        [Option('s', "syntax", SetName = "syntax", HelpText = "Syntax check an uncompiled string, uncompiled .gsc file, or directory containing main.gsc in it's root")]
+        [Option('s', "syntax", SetName = "syntax", HelpText = "Syntax check an uncompiled .gsc file or directory")]
         public string SyntaxCheckPath { get; set; }
 
-        [Option('c', "compile", SetName = "compile", HelpText = "Compile a string, file, or directory")]
+        [Option('c', "compile", SetName = "compile", HelpText = "Compile a .gsc file or directory containing main.gsc in it's root")]
         public string CompilePath { get; set; }
 
-        [Option('i', "inject", SetName = "inject", HelpText = "Inject an uncompiled string, uncompiled or compiled .gsc file, or a directory containing main.gsc in it's root")]
+        [Option('i', "inject", SetName = "inject", HelpText = "Inject an uncompiled .gsc file or directory containing main.gsc in it's root")]
         public string InjectPath { get; set; }
         
         [Option("compiled", SetName = "inject", HelpText = "Whether the .gsc file to be injected is compiled")]
